c8fdf435c94f56f2f7275dff451c4761c4b7791c
kimsehwanestat <sktpghks138@gmail.com>
2020-09-11 02:42:41 +0900

    second weeks done

---
+++ secondWeek/Hello.java
@@ -0,0 +1,12 @@
+package secondWeek;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int x = 3;
+        int y = 2;
+        double result = (double) x/ (double) y;
+        System.out.println(result);
+    }
+    
+}
