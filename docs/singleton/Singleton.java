733adff4db8b6c2a01bc82469bf2bedaef2ce119
kimsehwanestat <sktpghks138@gmail.com>
2020-10-29 22:54:16 +0900

    add comment to singleton pattern

--- docs/singleton/Singleton.java
+++ docs/singleton/Singleton.java
@@ -1,20 +1,25 @@
 public class Singleton {
     private static Singleton singleton = new Singleton();
+    // 정적 필드에 Singleton 생성자를 이용해서 singleton 객체를 한번 생성한다
 
-    private Singleton() {}
-    
+    private Singleton() {};
+    // 생성자는 외부 접근이 불가능하게 private 으로 접근제한한다
+
     static Singleton getInstance() {
         return singleton;
     }
-    public static void main(String[] args) {
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
+    // 스태틱 메서드로 유일한 인스턴스를 리턴하는 스태틱 메서드를 작성한다.
+    //static 리턴타입(싱글턴 객체의 타입이 리턴) getInstance 메서드 이름
 
-        if (obj1 == obj2) {
-            System.out.println("같은 싱글턴 객체당");
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+
+        if (a == b) {
+            System.out.println("같은 인스턴스");
         }
         else {
-            System.out.println("다른 싱긍턴 객체당!");
+            System.out.println("다른 인스턴스");
         }
     }
 }
